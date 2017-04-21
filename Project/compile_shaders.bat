561ac8c3c7f6b3c71007ed0540b898f900a88cf4
codergirl42 <marissa@galacticsoft.net>
2017-04-21 16:20:21 -0700

    Updated License to Apache 2.0

--- Project/compile_shaders.bat
+++ Project/compile_shaders.bat
@@ -1,15 +1,11 @@
 @ECHO OFF
 SETLOCAL
 
-REM Copyright 2016 Intel Corporation All Rights Reserved
-REM 
-REM Intel makes no representations about the suitability of this software for any purpose.
-REM THIS SOFTWARE IS PROVIDED ""AS IS."" INTEL SPECIFICALLY DISCLAIMS ALL WARRANTIES,
-REM EXPRESS OR IMPLIED, AND ALL LIABILITY, INCLUDING CONSEQUENTIAL AND OTHER INDIRECT DAMAGES,
-REM FOR THE USE OF THIS SOFTWARE, INCLUDING LIABILITY FOR INFRINGEMENT OF ANY PROPRIETARY
-REM RIGHTS, AND INCLUDING THE WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-REM Intel does not assume any responsibility for any errors which may appear in this software
-REM nor any responsibility to update it.
+REM /////////////////////////////////////////////////////////////////////////////////////////////
+REM // Copyright 2017 Intel Corporation
+REM //
+REM // Licensed under the Apache License, Version 2.0 (the "License");REM // you may not use this file except in compliance with the License.REM // You may obtain a copy of the License atREM //REM // http://www.apache.org/licenses/LICENSE-2.0REM //REM // Unless required by applicable law or agreed to in writing, softwareREM // distributed under the License is distributed on an "AS IS" BASIS,REM // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.REM // See the License for the specific language governing permissions andREM // limitations under the License.
+REM /////////////////////////////////////////////////////////////////////////////////////////////
 
 if [%1] == [] (
     echo No arguments provided! Please specify folder number.
